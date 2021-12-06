a685b5cd26ec9649292ae1d6562efa6cc9bd2ab7
djonatanpr <djonatanpr@garena.com>
2021-12-07 00:28:38 +0800

    Add gen for towns

--- towns/gen/gen.cpp
+++ towns/gen/gen.cpp
@@ -2,15 +2,77 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void randomR(int N) {
-  vector<string> R(N, string(N, '0'));
+// Only the upper-right part of R is read, the rest is mirrored
+void printR(vector<vector<int>> R) {
+  for (int i = 0; i < static_cast<int>(R.size()); ++i) {
+    for (int j = 0; j < i; ++j) {
+      printf("%c", 1 - R[j][i] + '0');
+    }
+    printf("0");
+    for (int j = i + 1; j < static_cast<int>(R[i].size()); ++j) {
+      printf("%c", R[i][j] + '0');
+    }
+    printf("\n");
+  }
+}
+
+vector<vector<int>> randomR(int N) {
+  vector R(N, vector(N, 0));
   for (int i = 0; i < N; ++i) {
     for (int j = i + 1; j < N; ++j) {
-      R[i][j] = rnd.next('0', '1');
-      R[j][i] = '1' - R[i][j] + '0'; 
+      R[i][j] = rnd.next(2);
     }
-    printf("%s\n", R[i].c_str());
   }
+  return R;
+}
+
+vector<vector<int>> hasNoOutgoingR(int N, int pivot=-1) {
+  if (pivot == -1) {
+    pivot = rnd.next(N);
+  }
+  auto R = randomR(N);
+  for (int i = 0; i < N; ++i) {
+    if (i < pivot) {
+      R[i][pivot] = 1;
+    } else if (i > pivot) {
+      R[pivot][i] = 0;
+    }
+  }
+  return R;
+}
+
+vector<vector<int>> hasOneOutgoingR(int N, int pivot=-1) {
+  if (pivot == -1) {
+    pivot = rnd.next(N);
+  }
+  auto R = hasNoOutgoingR(N, pivot);
+  int u = -1;
+  do {
+    u = rnd.next(N);
+  } while (u == pivot);
+
+  if (u < pivot) {
+    R[u][pivot] ^= 1;
+  } else {
+    R[pivot][u] ^= 1;
+  }
+  return R;
+}
+
+vector<vector<int>> dagR(int N) {
+  vector R(N, vector(N, 0));
+  vector<int> perm(N);
+  iota(perm.begin(), perm.end(), 0);
+  shuffle(perm.begin(), perm.end());
+
+  for (int i = 0; i < N; ++i) {
+    for (int j = i + 1; j < N; ++j) {
+      int u = perm[i], v = perm[j];
+      R[u][v] = 1;
+      R[v][u] = 0;
+    }
+  }
+  return R;
 }
 
 int main(int argc, char* argv[]) {
@@ -23,7 +85,20 @@ int main(int argc, char* argv[]) {
   const char* strategy = argv[4];
   printf("%s\n", strategy);
 
-  if (strcmp(strategy, "manual") == 0 || strcmp(strategy, "must-sure") == 0) {
-    randomR(N);
+  if (strcmp(strategy, "manual") == 0) {
+    printR(randomR(N));
+  }
+
+  if (strcmp(strategy, "must-sure") == 0) {
+    const char* graph = argv[5];
+    if (strcmp(graph, "random") == 0) {
+      printR(randomR(N));
+    } else if (strcmp(graph, "has-no-outgoing") == 0) {
+      printR(hasNoOutgoingR(N));
+    } else if (strcmp(graph, "has-one-outgoing") == 0) {
+      printR(hasOneOutgoingR(N));
+    } else if (strcmp(graph, "dag") == 0) {
+      printR(dagR(N));
+    }
   }
 }
--- towns/grader/manager.cpp
+++ towns/grader/manager.cpp
@@ -90,6 +90,295 @@ class MustSureStrategy : public Strategy {
   std::vector<int> outdeg;
 };
 
+class GreedyOutdegStrategy : public Strategy {
+ public:
+  GreedyOutdegStrategy(int N, bool greedyMax): N(N), greedyMax(greedyMax) {
+    R.resize(N, std::vector<int>(N, -1));
+    unknown.resize(N, N - 1);
+    outdeg.resize(N);
+  }
+
+  bool check_road(int A, int B) override {
+    if (R[A][B] != -1) {
+      return R[A][B] == 1 ? true : false;
+    }
+
+    if (greedyMax != (outdeg[A] < outdeg[B])) {
+      addEdge(A, B);
+      return true;
+    }
+    addEdge(B, A);
+    return false;
+  }
+
+  bool is_correct(int town) override {
+    if (town == -1) {
+      for (int i = 0; i < N; ++i) {
+        if (outdeg[i] <= 1) {
+          return false;
+        }
+      }
+      return true;
+    } else {
+      return outdeg[town] + unknown[town] <= 1;
+    }
+  }
+
+ private:
+  int N;
+  bool greedyMax;
+  std::vector<std::vector<int>> R;
+  std::vector<int> outdeg;
+  std::vector<int> unknown;
+
+  void addEdge(int u, int v) {
+    R[u][v] = 1;
+    R[v][u] = 0;
+    --unknown[u]; --unknown[v];
+    ++outdeg[u];
+  }
+};
+
+class NearFoundStrategy : public Strategy {
+ public:
+  NearFoundStrategy(int N): N(N) {
+    R.resize(N, std::vector<int>(N, -1));
+    unknown.resize(N, N - 1);
+    outdeg.resize(N);
+  }
+
+  bool check_road(int A, int B) override {
+    if (R[A][B] != -1) {
+      return R[A][B] == 1 ? true : false;
+    }
+
+    if (outdeg[A] <= 1 && outdeg[B] <= 1) {
+      addEdge(A, B);
+      return true;
+    }
+
+    if (outdeg[A] <= 1 && outdeg[B] > 1) {
+      if (outdeg[A] + unknown[A] - 1 <= 1) {
+        addEdge(A, B);
+        return true;
+      }
+      addEdge(B, A);
+      return false;
+    }
+
+    if (outdeg[A] > 1 && outdeg[B] <= 1) {
+      if (outdeg[B] + unknown[B] - 1 <= 1) {
+        addEdge(B, A);
+        return false;
+      }
+      addEdge(A, B);
+      return true;
+    }
+
+    addEdge(A, B);
+    return true;
+  }
+
+  bool is_correct(int town) override {
+    // Answer should always be -1
+    if (town == -1) {
+      for (int i = 0; i < N; ++i) {
+        if (outdeg[i] <= 1) {
+          return false;
+        }
+      }
+      return true;
+    } else {
+      return outdeg[town] + unknown[town] <= 1;
+    }
+  }
+
+ private:
+  int N;
+  std::vector<std::vector<int>> R;
+  std::vector<int> outdeg;
+  std::vector<int> unknown;
+
+  void addEdge(int u, int v) {
+    R[u][v] = 1;
+    R[v][u] = 0;
+    --unknown[u]; --unknown[v];
+    ++outdeg[u];
+  }
+};
+
+class MaintainCycleStrategy : public Strategy {
+ public:
+  MaintainCycleStrategy(int N, bool deterministic=false) :
+      N(N), deterministic(deterministic) {
+    R.resize(N, std::vector<int>(N, -1));
+    unknown.resize(N, N - 1);
+    outdeg.resize(N);
+    adj.resize(N);
+  }
+
+  bool check_road(int A, int B) override {
+    if (R[A][B] != -1) {
+      return R[A][B] == 1 ? true : false;
+    }
+
+    if (unknown[B] - 1 + outdeg[B] <= 1) {
+      addEdge(B, A);
+      // Cycle may be removed
+      rebuildCycle();
+      return false;
+    }
+
+    if (unknown[A] - 1 + outdeg[A] <= 1) {
+      assert(unknown[A] + outdeg[A] == 2);
+      addEdge(A, B);
+      // Cycle may be removed
+      rebuildCycle();
+      return true;
+    }
+
+    if (outdeg[A] == 0 && outdeg[B] == 0) {
+      return randomAddEdge(A, B);
+    }
+
+    if (outdeg[A] == 0 && outdeg[B] == 1) {
+      addEdge(A, B);
+      // A new cycle may be formed
+      rebuildCycle();
+      return true;
+    }
+
+    if (outdeg[A] == 1 && outdeg[B] == 0) {
+      addEdge(B, A);
+      // A new cycle may be formed
+      rebuildCycle();
+      return false;
+    }
+
+    if (outdeg[A] == 1 && outdeg[B] == 1) {
+      if (cyclePart[A] && cyclePart[B]) {
+        bool ret = randomAddEdge(A, B);
+        // Cycle becomes smaller
+        rebuildCycle();
+        return ret;
+      }
+
+      if (cyclePart[A] && !cyclePart[B]) {
+        addEdge(B, A);
+        return false;
+      }
+
+      if (!cyclePart[A] && cyclePart[B]) {
+        addEdge(A, B);
+        return true;
+      }
+
+      if (!cyclePart[A] && !cyclePart[B]) {
+        return randomAddEdge(A, B);
+      }
+
+      assert(false);
+    }
+
+    if (outdeg[A] > 1 && outdeg[B] <= 1) {
+      addEdge(A, B);
+      return true;
+    }
+
+    if (outdeg[A] <= 1 && outdeg[B] > 1) {
+      addEdge(B, A);
+      return false;
+    }
+
+    assert(outdeg[A] > 1 && outdeg[B] > 1);
+    return randomAddEdge(A, B);
+  }
+
+  bool is_correct(int town) override {
+    // if your solution was <= 4N, then `town` should be -1
+    // otherwise, your solution must be at least 5N
+
+    if (town == -1) {
+      for (int i = 0; i < N; ++i) {
+        if (outdeg[i] <= 1) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    if (outdeg[town] + unknown[town] <= 1) {
+      return true;
+    }
+    return false;
+  }
+
+ private:
+  int N;
+  bool deterministic;
+  std::vector<std::vector<int>> R;
+  std::vector<int> outdeg;
+  std::vector<int> unknown;
+  std::vector<std::vector<int>> adj;
+  std::vector<bool> cyclePart;
+
+  void addEdge(int u, int v) {
+    adj[u].push_back(v);
+    R[u][v] = 1;
+    R[v][u] = 0;
+    --unknown[u]; --unknown[v];
+    ++outdeg[u];
+  }
+
+  bool randomAddEdge(int u, int v) {
+    if (deterministic || rnd.next(2)) {
+      addEdge(u, v);
+      return true;
+    }
+    addEdge(v, u);
+    return false;
+  }
+
+  // O(N)
+  void rebuildCycle() {
+    cyclePart.assign(N, false);
+    std::vector<bool> vis(N, false);
+    std::vector<bool> visTour(N, false);
+    for (int i = 0; i < N; ++i) {
+      if (vis[i] || outdeg[i] != 1) continue;
+
+      int u = i;
+      std::vector<int> tour;
+      while (outdeg[u] == 1) {
+        visTour[u] = true;
+        tour.push_back(u);
+        u = adj[u][0];
+
+        if (vis[u]) {
+          break;
+        }
+
+        if (visTour[u]) {
+          // Found cycle
+          while (tour.back() != u) {
+            cyclePart[tour.back()] = true;
+            vis[tour.back()] = true;
+            visTour[tour.back()] = false;
+            tour.pop_back();
+          }
+          cyclePart[u] = true;
+          break;
+        }
+      }
+
+      for (int u : tour) {
+        vis[u] = true;
+        visTour[u] = false;
+      }
+    }
+  }
+};
+
 inline FILE* openFile(const char* name, const char* mode) {
   FILE* file = fopen(name, mode);
   if (!file) {
@@ -128,6 +417,16 @@ int main(int argc, char *argv[]) {
     strategy.reset(new ManualStrategy(N));
   } else if (std::string(buffer) == "must-sure") {
     strategy.reset(new MustSureStrategy(N));
+  } else if (std::string(buffer) == "greedy-outdeg-min") {
+    strategy.reset(new GreedyOutdegStrategy(N, false));
+  } else if (std::string(buffer) == "greedy-outdeg-max") {
+    strategy.reset(new GreedyOutdegStrategy(N, true));
+  } else if (std::string(buffer) == "near-found") {
+    strategy.reset(new NearFoundStrategy(N));
+  } else if (std::string(buffer) == "maintain-cycle") {
+    strategy.reset(new MaintainCycleStrategy(N));
+  } else if (std::string(buffer) == "maintain-cycle-deterministic") {
+    strategy.reset(new MaintainCycleStrategy(N, true));
   } else {
     assert(false);
   }
