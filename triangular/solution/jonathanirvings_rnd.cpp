8f4592a7aa052ffed21b56d64bd3d529abed5265
Jonathan Irvin Gunawan <jonathan.irvin@yahoo.com>
2021-12-06 11:53:07 +0800

    Address comments

--- triangular/solution/jonathanirvings_rnd.cpp
+++ triangular/solution/jonathanirvings_rnd.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-constexpr int kNumTries = 500000;
+constexpr int kMaxIterations = 500000;
+constexpr double kMaxRuntimeSecs = 0.9;
 constexpr int kRandomSeed = 477444;
 
 int main() {
@@ -18,8 +19,13 @@ int main() {
   vector<int> A(N);
   iota(A.begin(), A.end(), 1);
   pair<int, vector<int>> answer = make_pair(0, vector<int>({}));
-  for (int tries = 0; tries < kNumTries; ++tries) {
-    random_shuffle(A.begin(), A.end());
+  mt19937 rng(kRandomSeed);
+  clock_t tStart = clock();
+  for (int tries = 0;
+       tries < kMaxIterations &&
+           clock() - tStart < kMaxRuntimeSecs * CLOCKS_PER_SEC;
+       ++tries) {
+    shuffle(A.begin(), A.end(), rng);
     int value = 0;
     for (int i = 0; i < N; ++i) {
       value += triangular[A[i] + A[(i + 1) % N]] ? 1 : 0;
