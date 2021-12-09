439c47bd91a860d437ebdf237a923b65ac023064
Jonathan Irvin Gunawan <jonathanirvings@users.noreply.github.com>
2021-12-10 01:04:46 +0800

    Follow ioi21 compile script (#16)

--- aplusb/public/cpp/compile.sh
+++ aplusb/public/cpp/compile.sh
@@ -3,4 +3,4 @@
 problem=aplusb
 grader_name=grader
 
-g++ -std=gnu++17 -O2 -pipe -o "${problem}" "${grader_name}.cpp" "${problem}.cpp"
+g++ -std=gnu++17 -O2 -Wall -pipe -static -o "${problem}" "${grader_name}.cpp" "${problem}.cpp"
